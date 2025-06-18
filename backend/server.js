a7fc9334a9482c65ac095f8cb29db84d4a5541d1
Nick Kelley <125415952+nksz6@users.noreply.github.com>
2025-06-18 11:25:54 -0500

    fixed pass bug

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: 'landscaping_calculator',
-  ***REMOVED***: 'YourSecret***REMOVED***', // IM***REMOVED***ANT: Use your actual ***REMOVED***
+  ***REMOVED***: '***REMOVED***', // IM***REMOVED***ANT: Use your actual ***REMOVED***
   port: 5432,
 });
 
