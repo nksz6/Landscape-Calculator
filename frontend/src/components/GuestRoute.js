f2ba81c3beb191cccef44a540674de1e5abb6cff
Nick Kelley <125415952+nksz6@users.noreply.github.com>
2025-07-12 21:05:44 -0500

    implemented guest routes

---
+++ frontend/src/components/GuestRoute.js
@@ -0,0 +1,21 @@
+// In src/components/GuestRoute.js
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+// This component will wrap our guest-only views (Login, Registration)
+function GuestRoute({ children }) {
+    const { user } = useAuth();
+
+    if (user) {
+        // If the user IS logged in, don't show them the guest page.
+        // Instead, redirect them to the home/dashboard page.
+        return <Navigate to="/home" />;
+    }
+
+    // If the user is NOT logged in (i.e., they are a guest),
+    // render the child component they were trying to access (the Login or Registration page).
+    return children;
+}
+
+export default GuestRoute;
